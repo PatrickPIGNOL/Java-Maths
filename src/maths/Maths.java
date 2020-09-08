4d39ac9ac56467dcb92141e4d7be646445bdb238
Patrick PIGNOL <patrick.pignol@gmail.com>
2020-09-09 01:39:31 +0200

    Update Maths.java

--- src/maths/Maths.java
+++ src/maths/Maths.java
@@ -2,35 +2,6 @@ package maths;
 
 public class Maths 
 {
-	public static int mPGCD(int pDividende, int pDiviseur)
-	{
-		int vDividende;
-		int vDiviseur;	
-		int vResult;
-		int vReste;	
-		
-		if(pDividende < pDiviseur)
-		{
-			vDividende = pDiviseur;
-			vDiviseur = pDividende;
-		}
-		else
-		{
-			vDividende = pDividende;
-			vDiviseur = pDiviseur;
-		}
-		
-		do
-		{	
-			vResult = vDiviseur;
-			vReste = vDividende % vDiviseur;
-			vDividende = vDiviseur;
-			vDiviseur = vReste;
-		}while(vReste > 0);
-		
-		return vResult;
-	}
-	
 	public static long mPGCD(long pDividende, long pDiviseur)
 	{
 		long vDividende;
@@ -60,13 +31,12 @@ public class Maths
 		return vResult;
 	}
 	
-	public static int mPPCM(int pA, int pB)
-	{
-		return Math.abs(pA * pB) / Maths.mPGCD(pA, pB);
-	}
-	
 	public static long mPPCM(long pA, long pB)
 	{
 		return Math.abs(pA * pB) / Maths.mPGCD(pA, pB);
 	}
+	
+	public static double mHypothenuse()
+	//a2 = b2 + c2 -2bc * cos(A);
+	//A/sin(A) = b/sin(B) = c/sin(C);
 }
